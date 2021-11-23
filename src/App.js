c92cc7adb17b7d2582056a6a8aa3f6450edb9f33
Zainal Abidin <zainalabidinhsc@gmail.com>
2021-11-23 16:42:29 +0700

    config color + font

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createMuiTheme, ThemeProvider } from "@mui/material";
+import { Typography } from "@mui/material";
 
-function App() {
+// Custom Themes Material UI
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#345B63",
+      light: "#d4ecdd",
+      dark: "#112031",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#152D35",
+    },
+  },
+  typography: {
+    fontFamily: "Poppins",
+    fontWeightLight: 200,
+    fontWeightSemiBold: 400,
+    fontWeightBold: 600,
+  },
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Typography color="primary" fontWeight="fontWeightBold" variant="h1">
+        Final Project 2
+      </Typography>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
